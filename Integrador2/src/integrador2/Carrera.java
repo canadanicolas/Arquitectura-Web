b282b04b420c8a4bf1dc5b18773a8be1198329e2
canadanicolas <54717064+canadanicolas@users.noreply.github.com>
2021-09-20 22:17:25 -0300

    Subido integrador 2

---
+++ Integrador2/src/integrador2/Carrera.java
@@ -0,0 +1,68 @@
+package integrador2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Carrera {
+	
+	@Id //@GeneratedValue (strategy = GenerationType.AUTO)
+	int id;
+	
+	@Column(nullable = false)
+	String nombre;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "estudiante") 
+	List <CarreraEstudiante> estudiantes;
+	
+	public Carrera(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.estudiantes = new ArrayList<>();
+	}
+	
+	/*----------------------------------------------Getters y Setters----------------------------------------------*/
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<CarreraEstudiante> getEstudiantes() {
+		return new ArrayList<>(estudiantes);
+	}
+
+	public void setEstudiantes(List<CarreraEstudiante> estudiantes) {
+		this.estudiantes = estudiantes;
+	}
+	
+	/*---------------------------------------------- Otros Metodos ----------------------------------------------*/
+
+	public void addEstudiante(CarreraEstudiante estudiante) {
+		this.estudiantes.add(estudiante);
+	}
+	
+	@Override
+	public String toString() {
+		return "Carrera [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+}
---
+++ Integrador2/src/integrador2/insert.java
@@ -0,0 +1,52 @@
+package integrador2;
+
+import java.time.LocalDate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class insert {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Integrador2");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		Estudiante e1 = new Estudiante (39435576, "Nicolas", "Cañada", 25, "masculino", "Necochea", 1);
+		Estudiante e2 = new Estudiante (123456789, "Jose", "Perez", 75, "masculino", "Napoleofu", 2);
+		
+		Carrera c1 = new Carrera(4, "Tudai");
+		Carrera c2 = new Carrera(5, "Tupar");
+		
+		CarreraEstudiantePk cepk1 = new CarreraEstudiantePk(e1.getDocumento(), c1.getId());
+		CarreraEstudiantePk cepk2 = new CarreraEstudiantePk(e2.getDocumento(), c2.getId());
+		
+		CarreraEstudiante ce1 = new CarreraEstudiante(cepk1, e1, c1, LocalDate.now(), LocalDate.of(2023, 5, 7));
+		CarreraEstudiante ce2 = new CarreraEstudiante(cepk2, e2, c2, LocalDate.now(), LocalDate.of(2023, 5, 7));
+		
+		em.persist(e1);
+		em.persist(e2);
+		
+		em.persist(c1);
+		em.persist(c2);
+		
+		em.persist(ce1);
+		em.persist(ce2);
+		
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
+
+	/*public static void insertCarreraEstudiante(Estudiante e, Carrera c, EntityManager em) {
+		CarreraEstudiantePk cepk = new CarreraEstudiantePk(e.getDocumento(), c.getId());
+		System.out.println(cepk.getEstudianteId()+"-"+cepk.getCarreraId());
+		CarreraEstudiante ce = new CarreraEstudiante(cepk, e, c, LocalDate.now(), LocalDate.of(2023, 5, 7));
+		System.out.println(ce.getEstudiante().getDocumento()+"-"+ce.getCarrera().getId()+"-"+ce.getFechaIngreso()+"-"+ce.getFechaEgreso());
+		em.persist(ce);
+
+	}*/
+
+}
